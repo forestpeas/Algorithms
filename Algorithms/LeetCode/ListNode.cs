e6b261195f19c89c23d6b72bd1b3d2a65d47ac66
forestpeas <psholii@outlook.com>
2019-02-19 23:15:38 +0800

    Add Two Numbers

---
+++ Algorithms/LeetCode/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace Algorithms.LeetCode
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
