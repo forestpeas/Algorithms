1e3cb1ebea9b4395f75ab86aee1b27f984632acc
forestpeas <psholii@outlook.com>
2019-03-26 21:45:19 +0800

    Selection sort

---
+++ Algorithms/Sorting/SelectionSort.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static Algorithms.Sorting.Utils;
+
+namespace Algorithms.Sorting
+{
+    public class SelectionSort
+    {
+        public static void Sort<T>(T[] array) where T : IComparable
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j].CompareTo(array[min]) < 0) min = j;
+                }
+                Swap(array, i, min);
+            }
+        }
+    }
+}
---
+++ Algorithms/Sorting/Utils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Algorithms.Sorting
+{
+    internal class Utils
+    {
+        internal static void Swap<T>(T[] array, int i, int j) where T : IComparable
+        {
+            T tmp = array[i];
+            array[i] = array[j];
+            array[j] = tmp;
+        }
+    }
+}
