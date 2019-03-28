9769d4342900448ffb0d4aac1aed4e17d9add860
forestpeas <psholii@outlook.com>
2019-03-28 20:49:46 +0800

    Add test

---
+++ Algorithms.Tests/Test.cs
@@ -0,0 +1,21 @@
+﻿using Algorithms.Sorting;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class Test
+    {
+        [Fact]
+        public void TestSort()
+        {
+            var random = new Random();
+            int[] array = Enumerable.Repeat(0, 100).Select(_ => random.Next()).ToArray();
+            int[] arrayCopy = array.ToArray();
+            Array.Sort(arrayCopy);
+            SelectionSort.Sort(array);
+            Assert.True(Enumerable.SequenceEqual(array, arrayCopy));
+        }
+    }
+}
