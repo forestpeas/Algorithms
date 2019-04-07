08477b9b6ebdfc3aec7ebe3d80331ffd6a1193e4
forestpeas <psholii@outlook.com>
2019-04-07 21:58:11 +0800

    "3Sum Closest" added

---
+++ Algorithms/LeetCode/3Sum Closest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Algorithms.LeetCode
+{
+    public class ThreeSumClosestSolution
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            if (nums == null || nums.Length < 3) return 0;
+            Array.Sort(nums);
+            int difference = int.MaxValue;
+            int closest = 0;
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                //find "2Sum" in nums[i+1,...,nums.Length -1], target is target-nums[i]
+                if (i == 0 || nums[i] != nums[i - 1])
+                {
+                    int target2Sum = target - nums[i];
+                    for (int lo = i + 1, hi = nums.Length - 1; lo != hi;)
+                    {
+                        if (lo == i + 1 || nums[lo] != nums[lo - 1])
+                        {
+                            int current2Sum = nums[lo] + nums[hi];
+
+                            if (target2Sum > current2Sum) lo++;
+                            else if (target2Sum < current2Sum) hi--;
+                            else return target;
+
+                            int current3Sum = current2Sum + nums[i];
+                            int currentDifference = Math.Abs(current3Sum - target);
+                            if (currentDifference < difference)
+                            {
+                                closest = current3Sum;
+                                difference = currentDifference;
+                            }
+                        }
+                        else
+                        {
+                            lo++;
+                        }
+                    }
+                }
+            }
+            return closest;
+        }
+    }
+}
