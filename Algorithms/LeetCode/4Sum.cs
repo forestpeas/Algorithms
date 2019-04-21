15bbe37c79be458851fc605ccac71d5a940d8637
forestpeas <psholii@outlook.com>
2019-04-21 18:30:33 +0800

    "4Sum" complete

--- Algorithms/LeetCode/4Sum.cs
+++ Algorithms/LeetCode/4Sum.cs
@@ -3,33 +3,58 @@ using System.Collections.Generic;
 
 namespace Algorithms.LeetCode
 {
+    /* 18. 4Sum
+     * 
+     * Given an array nums of n integers and an integer target, are there elements a, b, c, and d in nums such that a + b + c + d = target? Find all unique quadruplets in the array which gives the sum of target.
+     * 
+     * Note: The solution set must not contain duplicate quadruplets.
+     * 
+     * Example:
+     * 
+     * Given array nums = [1, 0, -1, 0, -2, 2], and target = 0.
+     * 
+     * A solution set is:
+     * [
+     *   [-1,  0, 0, 1],
+     *   [-2, -1, 1, 2],
+     *   [-2,  0, 0, 2]
+     * ]
+     */
     public class FourSumSolution
     {
+        // Your runtime beats 58.84 % of csharp submissions.
+        // Your memory usage beats 86.11 % of csharp submissions.
         public IList<IList<int>> FourSum(int[] nums, int target)
         {
+            // solution below is similar to the 3Sum problem
             var ret = new List<IList<int>>();
             if (nums == null || nums.Length < 4) return ret;
             Array.Sort(nums);
             for (int j = 0; j < nums.Length - 3; j++)
             {
-                if (j != 0 && nums[j] == nums[j - 1]) continue;
+                if (j != 0 && nums[j] == nums[j - 1]) continue; // skip duplicates
                 //find "3Sum" in nums[j+1,...,nums.Length -1], target is target-nums[j]
                 for (int i = j + 1; i < nums.Length - 2; i++)
                 {
                     //find "2Sum" in nums[i+1,...,nums.Length -1], target is target-nums[j]-nums[i]
                     if (i == j + 1 || nums[i] != nums[i - 1])
                     {
-                        for (int lo = i + 1, hi = nums.Length - 1; lo != hi;)
+                        for (int lo = i + 1, hi = nums.Length - 1; lo < hi;)
                         {
-                            if (lo == i + 1 || nums[lo] != nums[lo - 1])
+                            if (target - nums[j] - nums[i] > nums[lo] + nums[hi])
                             {
-                                if (target - nums[j] - nums[i] > nums[lo] + nums[hi]) lo++;
-                                else if (target - nums[j] - nums[i] < nums[lo] + nums[hi]) hi--;
-                                else ret.Add(new List<int>() { nums[j], nums[i], nums[lo++], nums[hi] });
+                                while (++lo < nums.Length && nums[lo] == nums[lo - 1]) ; // skip duplicates
+                            }
+                            else if (target - nums[j] - nums[i] < nums[lo] + nums[hi])
+                            {
+                                while (--hi > 0 && nums[hi] == nums[hi + 1]) ; // skip duplicates
                             }
                             else
                             {
-                                lo++;
+                                ret.Add(new List<int>() { nums[j], nums[i], nums[lo], nums[hi] });
+                                // skip duplicates
+                                while (++lo < nums.Length && nums[lo] == nums[lo - 1]) ;
+                                while (--hi > 0 && nums[hi] == nums[hi + 1]) ;
                             }
                         }
                     }
