364549a96125c5bd5d31c8a37a8c183746819446
forestpeas <psholii@outlook.com>
2019-04-22 09:39:20 +0800

    "Generate Parentheses" added

---
+++ Algorithms/LeetCode/Generate Parentheses.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.LeetCode
+{
+    public class GenerateParentheses
+    {
+        public IList<string> GenerateParenthesis(int n)
+        {
+            List<string> result = new List<string>();
+            Backtrack(result, "", 0, 0, n);
+            return result;
+
+            void Backtrack(List<string> list, string str, int open, int close, int max)
+            {
+                if (str.Length == max * 2)
+                {
+                    list.Add(str);
+                    return;
+                }
+
+                if (open < max)
+                {
+                    Backtrack(list, str + "(", open + 1, close, max);
+                }
+                if (close < open)
+                {
+                    Backtrack(list, str + ")", open, close + 1, max);
+                }
+            }
+        }
+    }
+}
