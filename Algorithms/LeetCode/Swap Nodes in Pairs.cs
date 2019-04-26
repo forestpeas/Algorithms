518078f39857a1eb426091054dbcfb65cfb8960e
forestpeas <psholii@outlook.com>
2019-04-26 17:22:14 +0800

    "Swap Nodes in Pairs" complete

--- Algorithms/LeetCode/Swap Nodes in Pairs.cs
+++ Algorithms/LeetCode/Swap Nodes in Pairs.cs
@@ -2,14 +2,26 @@
 
 namespace Algorithms.LeetCode
 {
+    /* 24. Swap Nodes in Pairs
+     * 
+     * Given a linked list, swap every two adjacent nodes and return its head.
+     * You may not modify the values in the list's nodes, only nodes itself may be changed.
+     * 
+     * Example:
+     * 
+     * Given 1->2->3->4, you should return the list as 2->1->4->3.
+     */
     public class SwapNodesInPairs
     {
         // Your runtime beats 57.05 % of csharp submissions.
         // Your memory usage beats 27.96 % of csharp submissions.
         public ListNode SwapPairs(ListNode head)
         {
+            // The only difficulty of the problem is to carefully change the
+            // "next" of each node. It's not easy to write it right the 
+            // first time, so some tests are preferred.
             if (head == null) return null;
-            ListNode ret = head.next == null ? head : head.next;
+            ListNode ret = head.next ?? head;
             ListNode current = head;
             ListNode last = new ListNode(-1);
             while (current != null)
