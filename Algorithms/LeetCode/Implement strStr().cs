624a0b2d7bd2fde045cf910264c499709a86c5ec
forestpeas <psholii@outlook.com>
2019-04-30 14:12:57 +0800

    "Implement strStr()" added

---
+++ Algorithms/LeetCode/Implement strStr().cs
@@ -0,0 +1,48 @@
+﻿namespace Algorithms.LeetCode
+{
+    /* 28. Implement strStr()
+     * 
+     * Implement strStr().
+     * 
+     * Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+     * 
+     * Example 1:
+     * 
+     * Input: haystack = "hello", needle = "ll"
+     * Output: 2
+     * 
+     * Example 2:
+     * 
+     * Input: haystack = "aaaaa", needle = "bba"
+     * Output: -1
+     * Clarification:
+     * 
+     * What should we return when needle is an empty string? This is a great question to ask during an interview.
+     * 
+     * For the purpose of this problem, we will return 0 when needle is an empty string. This is consistent to C's strstr() and Java's indexOf().
+     */
+    public class ImplementStrStr
+    {
+        // Your runtime beats 99.93 % of csharp submissions.
+        // Your memory usage beats 49.27 % of csharp submissions.
+        public int StrStr(string haystack, string needle)
+        {
+            // Brute-force
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                for (int j = 0; ; j++)
+                {
+                    if (j == needle.Length)
+                    {
+                        return i;
+                    }
+                    if (haystack[i + j] != needle[j])
+                    {
+                        break;
+                    }
+                }
+            }
+            return -1;
+        }
+    }
+}
