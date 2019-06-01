7bf3259a302f8253dbad2300b9c50e6c50e63aa9
forestpeas <psholii@outlook.com>
2019-06-01 11:08:17 +0800

    "Combinations" add explanation

--- Algorithms/LeetCode/Combinations.cs
+++ Algorithms/LeetCode/Combinations.cs
@@ -27,6 +27,16 @@ namespace Algorithms.LeetCode
             Combine(new int[k], 0, 1);
             return results;
 
+            // Every “box”(a position in the result) has a range
+            // For example: Input: n = 4, k = 3
+            // 1,2,3
+            // 1,2,4
+            // 1,3,4
+            // 2,3,4
+            // The first box ranges from 1 to 2.
+            // The first box ranges from 2 to 3.
+            // The first box ranges from 3 to 4.
+            // "i" is the box number, "start" is the range's start point.
             void Combine(int[] result, int i, int start)
             {
                 if (i + 1 == k)
