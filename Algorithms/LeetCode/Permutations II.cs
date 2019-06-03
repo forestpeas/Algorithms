9617dd57749d2029581dce788d6eb67a871c90d6
forestpeas <psholii@outlook.com>
2019-06-03 09:11:17 +0800

    "Permutations II" added; add some explanations to "Permutations"

---
+++ Algorithms/LeetCode/Permutations II.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.LeetCode
+{
+    /* 47. Permutations II
+     * 
+     * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+     * 
+     * Example:
+     * 
+     * Input: [1,1,2]
+     * Output:
+     * [
+     *   [1,1,2],
+     *   [1,2,1],
+     *   [2,1,1]
+     * ]
+     */
+    public class PermutationsII
+    {
+        private readonly HashSet<int> fixedIndexes = new HashSet<int>();
+
+        public IList<IList<int>> PermuteUnique(int[] nums)
+        {
+            // Similar to "46. Permutations".
+            // For example: nums = [1,1,2]
+            // When the first "1" is fixed and subResults are permuted, we come to the second "1".
+            // But we can skip this one, because it is completely the same with the first one.
+            if (nums.Length == fixedIndexes.Count) return new List<int>[] { new List<int>() };
+            var results = new List<IList<int>>();
+            var permuted = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (fixedIndexes.Contains(i)) continue;
+                if (!permuted.Add(nums[i])) continue;
+                fixedIndexes.Add(i);
+                foreach (var subResult in PermuteUnique(nums))
+                {
+                    subResult.Add(nums[i]);
+                    results.Add(subResult);
+                }
+                fixedIndexes.Remove(i);
+            }
+            return results;
+        }
+    }
+}
--- Algorithms/LeetCode/Permutations.cs
+++ Algorithms/LeetCode/Permutations.cs
@@ -25,6 +25,13 @@ namespace Algorithms.LeetCode
 
         public IList<IList<int>> Permute(int[] nums)
         {
+            // Backtrack
+            // For example: nums = [1,2,3]
+            // 1.we have to choose one number from [1,2,3]. We choose 1 and 1 is fixed.
+            // 2.we have to choose one number from [2,3]. we choose 2 and [1,2] are fixed.
+            // 3.we choose 3 because it is all that it left, so we get a result: [1,2,3].
+            // Then we backtrack to the second step and choose 3 so that [1,3] are fixed.
+            // ...
             if (nums.Length == fixedIndexes.Count) return new List<int>[] { new List<int>() };
             var results = new List<IList<int>>();
             for (int i = 0; i < nums.Length; i++)
