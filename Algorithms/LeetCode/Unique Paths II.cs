7df3fd9ce404e4f511e1f202a90dd4380c9f83b3
forestpeas <psholii@outlook.com>
2019-06-17 11:24:30 +0800

    "UniquePathsII" make code shorter and complete explanations

--- Algorithms/LeetCode/Unique Paths II.cs
+++ Algorithms/LeetCode/Unique Paths II.cs
@@ -29,32 +29,23 @@
     {
         public int UniquePathsWithObstacles(int[][] obstacleGrid)
         {
-            // obstacleGrid[1][1] = 1
+            // Similar to "Problem 62. Unique Paths". The only difference is that
+            // if "obstacleGrid[i][j]" is 1, then mem[i][j] is 0.
+            // For example: obstacleGrid is a 4 x 4 grid. The only obstacle in it is obstacleGrid[1][1].
+            // "mem" should be:
             // 1  1  1  1
             // 1  0  1  2
             // 1  1  2  4
             // 1  2  4  8
             if (obstacleGrid.Length < 1) return 0;
             int[] mem = new int[obstacleGrid[0].Length];
-            mem[0] = obstacleGrid[0][0] == 1 ? 0 : 1;
-            for (int i = 1; i < mem.Length; i++)
-            {
-                mem[i] = obstacleGrid[0][i] == 1 ? 0 : mem[i - 1];
-            }
-
-            for (int i = 1; i < obstacleGrid.Length; i++)
+            mem[0] = 1;
+            for (int i = 0; i < obstacleGrid.Length; i++)
             {
                 mem[0] = obstacleGrid[i][0] == 1 ? 0 : mem[0];
                 for (int j = 1; j < mem.Length; j++)
                 {
-                    if (obstacleGrid[i][j] == 1)
-                    {
-                        mem[j] = 0;
-                    }
-                    else
-                    {
-                        mem[j] = mem[j] + mem[j - 1];
-                    }
+                    mem[j] = obstacleGrid[i][j] == 1 ? 0 : mem[j] + mem[j - 1];
                 }
             }
             return mem[mem.Length - 1];
