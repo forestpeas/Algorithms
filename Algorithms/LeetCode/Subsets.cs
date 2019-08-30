feb9feea7204795908153faead28ee8845dfb6ff
forestpeas <psholii@outlook.com>
2019-08-30 09:20:37 +0800

    "Subsets" add bit manipulation solution

--- Algorithms/LeetCode/Subsets.cs
+++ Algorithms/LeetCode/Subsets.cs
@@ -58,5 +58,36 @@ namespace Algorithms.LeetCode
             }
             return subsets;
         }
+
+        public IList<IList<int>> Subsets2(int[] nums)
+        {
+            // To get a subset, for each number we choose whether to put it in the subset.
+            // For example: nums = [1,2,3], there are 2^3 = 8 subsets.
+            // Each number in 0~7 correspond to a subset. 
+            // The 1-bit means the number in this position should be in the subset.
+            // 000 // Nothing should be chosen.
+            // 001 // First number should be chosen.
+            // 010 // Second number should be chosen.
+            // 011 // First and second number should be chosen.
+            // 100 // Third number should be chosen.
+            // 101 // ...
+            // 110
+            // 111
+            int n = 1 << nums.Length;
+            var subsets = new List<IList<int>>();
+            for (int i = 0; i < n; i++)
+            {
+                var subset = new List<int>();
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (((i >> j) & 1) == 1) // Whether the j-th bit in i is 1.
+                    {
+                        subset.Add(nums[j]);
+                    }
+                }
+                subsets.Add(subset);
+            }
+            return subsets;
+        }
     }
 }
