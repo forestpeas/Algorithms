5094f3e2030ebcae4138968abee98cc03ed836e7
forestpeas <psholii@outlook.com>
2019-09-09 09:19:13 +0800

    "Palindrome Partitioning" added

---
+++ Algorithms/LeetCode/Palindrome Partitioning.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.LeetCode
+{
+    /* 131. Palindrome Partitioning
+     * 
+     * Given a string s, partition s such that every substring of the partition is a palindrome.
+     * Return all possible palindrome partitioning of s.
+     * 
+     * Example:
+     * 
+     * Input: "aab"
+     * Output:
+     * [
+     *   ["aa","b"],
+     *   ["a","a","b"]
+     * ]
+     */
+    public class PalindromePartitioning
+    {
+        private readonly Dictionary<string, List<IList<string>>> _mem = new Dictionary<string, List<IList<string>>>();
+
+        public IList<IList<string>> Partition(string s)
+        {
+            // Recursive solution with memoization.
+            if (_mem.TryGetValue(s, out var value)) return value;
+
+            var result = new List<IList<string>>();
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                if (IsPalindrome(s, i, s.Length - 1))
+                {
+                    if (i == 0)
+                    {
+                        result.Add(new List<string>() { s });
+                        break;
+                    }
+                    foreach (var item in Partition(s.Substring(0, i)))
+                    {
+                        var copy = item.ToList();
+                        copy.Add(s.Substring(i));
+                        result.Add(copy);
+                    }
+                }
+            }
+
+            _mem.Add(s, result);
+            return result;
+        }
+
+        private bool IsPalindrome(string s, int start, int end)
+        {
+            while (start < end)
+            {
+                if (s[start++] != s[end--]) return false;
+            }
+
+            return true;
+        }
+    }
+}
