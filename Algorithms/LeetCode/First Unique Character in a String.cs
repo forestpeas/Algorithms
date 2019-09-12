69223d6775ef01c17f36996f6fe0125593d25242
forestpeas <psholii@outlook.com>
2019-09-12 10:48:33 +0800

    "First Unique Character in a String" added

---
+++ Algorithms/LeetCode/First Unique Character in a String.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.LeetCode
+{
+    /* 387. First Unique Character in a String
+     * 
+     * Given a string, find the first non-repeating character in it and return it's index.
+     * If it doesn't exist, return -1.
+     * 
+     * Examples:
+     * 
+     * s = "leetcode"
+     * return 0.
+     * 
+     * s = "loveleetcode",
+     * return 2.
+     * 
+     * Note: You may assume the string contain only lowercase letters.
+     */
+    public class FirstUniqueCharacterInAString
+    {
+        public int FirstUniqChar(string s)
+        {
+            var counter = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                counter[c] = counter.GetValueOrDefault(c) + 1;
+            }
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (counter[s[i]] == 1) return i;
+            }
+
+            return -1;
+        }
+    }
+}
