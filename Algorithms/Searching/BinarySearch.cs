43f777e7cc25c1ceef82355bcb426b6efa4c9844
forestpeas <psholii@outlook.com>
2019-09-28 10:39:21 +0800

    BinarySearch added

---
+++ Algorithms/Searching/BinarySearch.cs
@@ -0,0 +1,63 @@
+﻿namespace Algorithms.Searching
+{
+    public class BinarySearch
+    {
+        /* There are several typical templates for binary search.
+         * We need to consider some corner cases: 
+         * When array has only one element, make sure we can enter the loop.
+         * When lo + 1 = hi, mid = lo.
+         */
+
+        public static int Search1(int[] array, int target)
+        {
+            // Search range is [lo, hi].
+            int lo = 0;
+            int hi = array.Length - 1;
+            while (lo <= hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (array[mid] > target)
+                {
+                    hi = mid - 1;
+                }
+                else if (array[mid] < target)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+
+        public static int Search2(int[] array, int target)
+        {
+            // Search range is [lo, hi).
+            int lo = 0;
+            int hi = array.Length;
+            while (lo < hi)
+            {
+                int mid = lo + (hi - lo) / 2;
+                if (array[mid] > target)
+                {
+                    // When hi has been assigned mid, the search range is still [lo,hi).
+                    // Because hi was already checked by mid.
+                    hi = mid;
+                }
+                else if (array[mid] < target)
+                {
+                    lo = mid + 1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
