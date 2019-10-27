ed7038f76c7ecf22de4fadec89651cbae7b7260a
forestpeas <psholii@outlook.com>
2019-10-27 16:27:23 +0800

    Problem 1238 added

---
+++ Algorithms/LeetCode/Circular Permutation in Binary Representation.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms.LeetCode
+{
+    public class Circular_Permutation_in_Binary_Representation
+    {
+        public IList<int> CircularPermutation(int n, int start)
+        {
+            // Similar to "89. Gray Code".
+            // Flip the lsd, then the second significant digit...
+            var result = new List<int>() { start };
+            for (int i = 0; i < n; i++)
+            {
+                int mask = 1 << i;
+                if ((start & mask) == mask)
+                {
+                    for (int j = result.Count - 1; j >= 0; j--)
+                    {
+                        result.Add(result[j] & ~mask);
+                    }
+                }
+                else
+                {
+                    for (int j = result.Count - 1; j >= 0; j--)
+                    {
+                        result.Add(result[j] | mask);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
