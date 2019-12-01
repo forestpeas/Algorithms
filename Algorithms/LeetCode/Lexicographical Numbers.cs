781be1f505f79dea9aeebabf739fd6692f299134
forestpeas <psholii@outlook.com>
2019-12-01 12:02:34 +0800

    "386. Lexicographical Numbers" added

---
+++ Algorithms/LeetCode/Lexicographical Numbers.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms.LeetCode
+{
+    /* 386. Lexicographical Numbers
+     * 
+     * Given an integer n, return 1 - n in lexicographical order.
+     * For example, given 13, return: [1,10,11,12,13,2,3,4,5,6,7,8,9].
+     * 
+     * Please optimize your algorithm to use less time and space. The input size may be as large as 5,000,000.
+     */
+    public class Lexicographical_Numbers
+    {
+        public IList<int> LexicalOrder(int n)
+        {
+            // Observe the pattern.
+            // 1
+            // 10
+            // 100
+            // 101
+            // 102
+            // 103
+            // ...
+            // 109
+            // 11
+            // 110
+            // ...
+            // 199
+            // 2
+            // 20
+            // 200
+            // ...
+            var result = new List<int>();
+            int num = 1;
+            while (result.Count < n)
+            {
+                // Try to put as many zeroes as possible to the end of the number.
+                while (num <= n)
+                {
+                    result.Add(num);
+                    num = num * 10;
+                }
+                num = num / 10;
+
+                // Increment the number until a carry.
+                while (true)
+                {
+                    num++;
+                    if (num % 10 == 0) break;
+                    if (num <= n) result.Add(num);
+                }
+
+                // Try to remove as many zeroes as possible from the end of the number.
+                while (num % 10 == 0)
+                {
+                    num = num / 10;
+                }
+            }
+
+            return result;
+        }
+
+        public IList<int> LexicalOrder2(int n)
+        {
+            return Enumerable.Range(1, n).OrderBy(i => i.ToString()).ToArray();
+        }
+    }
+}
