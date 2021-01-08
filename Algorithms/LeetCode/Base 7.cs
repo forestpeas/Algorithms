1a8107a77b855af56e57adb9682c5c470721f237
forestpeas <psholii@outlook.com>
2021-01-08 18:20:12 +0800

    "504. Base 7" added

---
+++ Algorithms/LeetCode/Base 7.cs
@@ -0,0 +1,24 @@
+﻿namespace Algorithms.LeetCode
+{
+    /* 504. Base 7
+     * 
+     * Given an integer, return its base 7 string representation.
+     * 
+     * Example 1:
+     * Input: 100
+     * Output: "202"
+     * 
+     * Example 2:
+     * Input: -7
+     * Output: "-10"
+     */
+    public class Base_7
+    {
+        public string ConvertToBase7(int num)
+        {
+            if (num < 0) return "-" + ConvertToBase7(-num);
+            if (num < 7) return num.ToString();
+            return ConvertToBase7(num / 7) + (num % 7);
+        }
+    }
+}
