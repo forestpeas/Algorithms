1c2a8c8e1efe106710013dfd29b90bbebf0b8578
forestpeas <psholii@outlook.com>
2021-02-05 17:56:06 +0800

    "538. Convert BST to Greater Tree" added

---
+++ Algorithms/LeetCode/Convert BST to Greater Tree.cs
@@ -0,0 +1,31 @@
+﻿using Algorithms.DataStructures;
+
+namespace Algorithms.LeetCode
+{
+    /* 538. Convert BST to Greater Tree
+     * 
+     * Given the root of a Binary Search Tree (BST), convert it to a Greater Tree such that
+     * every key of the original BST is changed to the original key plus sum of all keys
+     * greater than the original key in BST.
+     * 
+     * https://leetcode.com/problems/convert-bst-to-greater-tree/
+     */
+    public class Convert_BST_to_Greater_Tree
+    {
+        public TreeNode ConvertBST(TreeNode root)
+        {
+            int sum = 0;
+            Dfs(root);
+            return root;
+
+            void Dfs(TreeNode node)
+            {
+                if (node == null) return;
+                Dfs(node.right);
+                sum += node.val;
+                node.val = sum;
+                Dfs(node.left);
+            }
+        }
+    }
+}
