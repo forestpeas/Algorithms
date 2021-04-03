71b62e1c5ea3c233d0358db3563038fc3799cb7e
forestpeas <psholii@outlook.com>
2021-04-03 11:38:28 +0800

    "684. Redundant Connection" added

--- Algorithms/DataStructures/UnionFind.cs
+++ Algorithms/DataStructures/UnionFind.cs
@@ -2,15 +2,15 @@
 {
     public class UnionFind
     {
-        private readonly int[] _id;
+        private readonly int[] _parent;
 
         public UnionFind(int count)
         {
             Count = count;
-            _id = new int[count];
+            _parent = new int[count];
             for (int i = 0; i < count; i++)
             {
-                _id[i] = i;
+                _parent[i] = i;
             }
         }
 
@@ -20,7 +20,7 @@
 
         public int Find(int p)
         {
-            while (p != _id[p]) p = _id[p];
+            while (p != _parent[p]) p = _parent[p];
             return p;
         }
 
@@ -30,8 +30,53 @@
             int pRoot = Find(p);
             int qRoot = Find(q);
             if (pRoot == qRoot) return false;
-            _id[pRoot] = qRoot;
+            _parent[pRoot] = qRoot;
             Count--;
+            return true;
+        }
+    }
+
+    public class BalancedUnionFind
+    {
+        private readonly int[] _parent;
+        private readonly int[] _height;
+
+        public BalancedUnionFind(int count)
+        {
+            _parent = new int[count];
+            for (int i = 0; i < count; i++)
+            {
+                _parent[i] = i;
+            }
+            _height = new int[count];
+        }
+
+        public int Find(int p)
+        {
+            while (p != _parent[p]) p = _parent[p];
+            return p;
+        }
+
+        public bool Union(int p, int q)
+        {
+            int pRoot = Find(p), qRoot = Find(q);
+            if (pRoot == qRoot)
+            {
+                return false;
+            }
+            else if (_height[pRoot] < _height[qRoot])
+            {
+                _parent[pRoot] = qRoot;
+            }
+            else if (_height[pRoot] > _height[qRoot])
+            {
+                _parent[qRoot] = pRoot;
+            }
+            else
+            {
+                _parent[qRoot] = pRoot;
+                _height[pRoot]++;
+            }
             return true;
         }
     }
