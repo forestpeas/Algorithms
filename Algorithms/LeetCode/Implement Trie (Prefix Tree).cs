6801e70da9b4ad5da5b3b3ef4dffbbd45e1fa608
forestpeas <psholii@outlook.com>
2021-04-03 12:01:28 +0800

    prefix tree iterative version

--- Algorithms/LeetCode/Implement Trie (Prefix Tree).cs
+++ Algorithms/LeetCode/Implement Trie (Prefix Tree).cs
@@ -19,17 +19,69 @@
      * You may assume that all inputs are consist of lowercase letters a-z.
      * All inputs are guaranteed to be non-empty strings.
      */
+
     public class Trie
     {
-        private readonly Node _root = new Node();
-
         private class Node
         {
             public bool IsEnd { get; set; } = false;
-            public Node[] Next { get; } = new Node[26];
+            public Node[] Next { get; } = new Node[256];
         }
 
-        /** Inserts a word into the trie. */
+        private readonly Node _root = new Node();
+
+        public void Insert(string word)
+        {
+            Node node = _root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (node.Next[word[i]] == null)
+                {
+                    node.Next[word[i]] = new Node();
+                }
+                node = node.Next[word[i]];
+            }
+            node.IsEnd = true;
+        }
+
+        private Node SearchPrefix(string word)
+        {
+            Node node = _root;
+            for (int i = 0; i < word.Length; i++)
+            {
+                node = node.Next[word[i]];
+                if (node == null)
+                {
+                    return null;
+                }
+            }
+            return node;
+        }
+
+        public bool Search(string word)
+        {
+            var node = SearchPrefix(word);
+            return node != null && node.IsEnd;
+        }
+
+        public bool StartsWith(string prefix)
+        {
+            var node = SearchPrefix(prefix);
+            return node != null;
+        }
+    }
+
+    // recursive implementation
+    public class Trie2
+    {
+        private class Node
+        {
+            public bool IsEnd { get; set; } = false;
+            public Node[] Next { get; } = new Node[256];
+        }
+
+        private readonly Node _root = new Node();
+
         public void Insert(string word)
         {
             Insert(_root, word, 0);
@@ -48,24 +100,22 @@
             return node;
         }
 
-        /** Returns if the word is in the trie. */
         public bool Search(string word)
         {
-            Node node = Search(_root, word, 0);
+            Node node = SearchPrefix(_root, word, 0);
             return node != null && node.IsEnd;
         }
 
-        private Node Search(Node node, string word, int i)
+        private Node SearchPrefix(Node node, string word, int i)
         {
             if (node == null) return null;
             if (i == word.Length) return node;
-            return Search(node.Next[word[i] - 'a'], word, i + 1);
+            return SearchPrefix(node.Next[word[i] - 'a'], word, i + 1);
         }
 
-        /** Returns if there is any word in the trie that starts with the given prefix. */
         public bool StartsWith(string prefix)
         {
-            Node node = Search(_root, prefix, 0);
+            Node node = SearchPrefix(_root, prefix, 0);
             return node != null;
         }
     }
