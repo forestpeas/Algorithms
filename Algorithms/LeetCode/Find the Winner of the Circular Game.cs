f9497f89bfbcc70d74e906893b2b8c161e422348
forestpeas <psholii@outlook.com>
2021-04-17 12:44:29 +0800

    "1823. Find the Winner of the Circular Game" added

---
+++ Algorithms/LeetCode/Find the Winner of the Circular Game.cs
@@ -0,0 +1,21 @@
+﻿namespace Algorithms.LeetCode
+{
+    /* 1823. Find the Winner of the Circular Game
+     *  
+     * https://leetcode.com/problems/find-the-winner-of-the-circular-game/
+     */
+    public class Find_the_Winner_of_the_Circular_Game
+    {
+        public int FindTheWinner(int n, int m)
+        {
+            // Josephus Problem
+            return F(n) + 1;
+
+            int F(int n)
+            {
+                if (n == 1) return 0;
+                return (F(n - 1) + m) % n;
+            }
+        }
+    }
+}
